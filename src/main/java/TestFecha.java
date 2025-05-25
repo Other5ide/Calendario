71de1e4e0f98308ce9bce93807a3a3f91c8e9736
Matías <124216748+Other5ide@users.noreply.github.com>
2025-05-25 17:13:12 -0400

    Initial commit

--- src/main/java/TestFecha.java
+++ src/main/java/TestFecha.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class TestFecha {
+    public static void main(String[] args) {
+        TodaysDate hoy = new TodaysDate();
+        hoy.printDateAndTime();
+    }
 }
